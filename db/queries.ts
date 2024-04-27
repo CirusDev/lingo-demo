7620f1731b5b7a2692e1f8922a17106495f12ecd
CirusDev <devcirus@gmail.com>
2024-04-27 12:06:30 +0200

    setting active course progress

--- db/queries.ts
+++ db/queries.ts
@@ -3,7 +3,7 @@ import { auth } from '@clerk/nextjs/server'
 import { cache } from 'react'
 
 import db from './drizzle'
-import { userProgress } from './schema'
+import { courses, userProgress } from './schema'
 
 export const getUserProgress = cache(async () => {
   const { userId } = auth()
@@ -26,3 +26,11 @@ export const getCourses = cache(async () => {
   return data
 })
 
+export const getCourseById = cache(async(courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId)
+    //TODO: populate units & lessons
+  })
+
+  return data
+})
